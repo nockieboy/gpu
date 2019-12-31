09ddc3bb3391e78b14fbb25eee9591077230045c
Jonathan Nock <nockieboy@gmail.com>
2019-12-31 13:12:48 +0000

    Data mux completed.

--- GPU_256char_font/Z80_bridge.v
+++ GPU_256char_font/Z80_bridge.v
@@ -14,96 +14,89 @@ module Z80_bridge (
 	input wire	gpu_rd_rdy,		// one-shot signal from mux that data is ready
 	
 	// output
-	
-	output reg	Z80_245data_dir,			// control level converter direction for data flow - HIGH = A -> B (toward FPGA)
-	output reg	[7:0]   Z80_rData,		// Z80 DATA bus to return data from GPU RAM to Z80
-	output reg	Z80_rData_ena,				// flag HIGH to write data back to Z80
-	output reg	Z80_245_oe,					// OE for 245 level translator (active LOW)
-	output reg	gpu_wr_ena,					// flag HIGH for 1 clock when writing to GPU RAM
-	output reg	gpu_rd_req,					// flag HIGH for 1 clock when reading from GPU RAM
-	output reg	[19:0]  gpu_addr,			// connect to Z80_addr in vid_osd_generator to address GPU RAM
-	output reg	[7:0]   gpu_wdata			// 8-bit data bus to GPU RAM in vid_osd_generator
+	output reg	Z80_245data_dir,	// control level converter direction for data flow - HIGH = A -> B (toward FPGA)
+	output reg	[7:0]  Z80_rData,	// Z80 DATA bus to return data from GPU RAM to Z80
+	output reg	Z80_rData_ena,		// flag HIGH to write data back to Z80
+	output reg	Z80_245_oe,			// OE for 245 level translator (active LOW)
+	output reg	gpu_wr_ena,			// flag HIGH for 1 clock when writing to GPU RAM
+	output reg	gpu_rd_req,			// flag HIGH for 1 clock when reading from GPU RAM
+	output reg	[19:0] gpu_addr,	// connect to Z80_addr in vid_osd_generator to address GPU RAM
+	output reg	[7:0]  gpu_wdata	// 8-bit data bus to GPU RAM in vid_osd_generator
 
 );
 
 parameter MEMORY_RANGE = 3'b011;	// Z80_addr[21:19] == 3'b011 targets the 512KB 'window' at 0x180000-0x1FFFFF
 parameter DELAY_CYCLES = 2;		// number of cycles to delay write for 245
 
-wire mem_window, Z80_mreq, Z80_write, Z80_read, Z80_nRead, Write_GPU_RAM, Read_GPU_RAM, GPU_data_oe;
+wire mem_window, Z80_mreq, Z80_write, Z80_read, Z80_nRead, Write_GPU_RAM, GPU_data_oe;
 wire Read_GPU_RAM_BEGIN, Read_GPU_RAM_END;
 
 reg last_Z80_WR	= 1'b0;  // keep these low on power-up, otherwise a read or write pulse may be triggered
 reg last_Z80_RD	= 1'b0;
 reg Z80_clk_delay;
-//reg data_hold		= 1'b0;	// used to latch the gpu_rd_rdy signal
 reg [9:0] Z80_write_sequencer;
 
 assign mem_window	= (Z80_addr[21:19] == MEMORY_RANGE);	// Define an active memory range
-assign Z80_mreq	= 	~Z80_MREQn && Z80_M1n;		// Define a bus memory access state
-assign Z80_write	= ~Z80_WRn && last_Z80_WR;		// Isolate a single write transaction
-assign Z80_read	= 	~Z80_RDn && last_Z80_RD;	// Isolate a single read transaction
-assign Z80_nRead	= 	Z80_RDn && ~last_Z80_RD;	// Isolate end of a read transaction
+assign Z80_mreq	= 	~Z80_MREQn && Z80_M1n;					// Define a bus memory access state
+assign Z80_write	= ~Z80_WRn && last_Z80_WR;					// Isolate a single write transaction
+assign Z80_read	= 	~Z80_RDn && last_Z80_RD;				// Isolate a single read transaction
+assign Z80_nRead	= 	Z80_RDn && ~last_Z80_RD;				// Isolate end of a read transaction
 
-assign Write_GPU_RAM	= mem_window && Z80_mreq && Z80_write;	// Define a GPU Write action
-//assign Read_GPU_RAM	= mem_window && Z80_mreq && Z80_read;	// Define a GPU Read action
-assign Read_GPU_RAM_BEGIN = mem_window && Z80_mreq && ~Z80_RDn && Z80_CLK && ~Z80_clk_delay && ~Z80_rData_ena;   // Define the beginning of a Z80 read request of GPU Ram.
-assign Read_GPU_RAM_END = Z80_RDn && ~last_Z80_RD;  // Define the time to end a GPU ram read
-assign GPU_data_oe	= mem_window && Z80_mreq && ~Z80_RDn;	// Define the time the GPU ouputs data onto the Z80 data bus
+assign Write_GPU_RAM			= mem_window && Z80_mreq && Z80_write;	// Define a GPU Write action
+assign Read_GPU_RAM_BEGIN	= mem_window && Z80_mreq && ~Z80_RDn && Z80_CLK && ~Z80_clk_delay && ~Z80_rData_ena;   // Define the beginning of a Z80 read request of GPU Ram.
+assign Read_GPU_RAM_END		= Z80_RDn && ~last_Z80_RD;					// Define the time to end a GPU ram read
+assign GPU_data_oe			= mem_window && Z80_mreq && ~Z80_RDn;	// Define the time the GPU ouputs data onto the Z80 data bus
 
-//assign gpu_rd_req = 1'b0;				// default gpu_rd_req to LOW
-//assign Z80_245_oe		= 1'b0;				// disable 245 output
-//assign Z80_rData_ena = 1'b0;			// set Z80 data pins to input on the FPGA
+// **********************************************************************************************************
 
-always @ (posedge GPU_CLK)
-begin
+always @ (posedge GPU_CLK) begin
 
 	Z80_write_sequencer[9:0] <= { Z80_write_sequencer[8:0], Write_GPU_RAM };
 
-	if ( Z80_write_sequencer[0] )                 Z80_245data_dir  <= 1'b1;					// set 245 dir toward FPGA
-	if ( Z80_write_sequencer[0] )                 Z80_rData_ena    <= 1'b0;					// set FPGA pins to input (should be by default)
+	if ( Z80_write_sequencer[0] )                 Z80_245data_dir  <= 1'b1;				// set 245 dir toward FPGA
+	if ( Z80_write_sequencer[0] )                 Z80_rData_ena    <= 1'b0;				// set FPGA pins to input (should be by default)
 
-	if ( Z80_write_sequencer[1] )                 Z80_245_oe       <= 1'b1;					// enable 245 output
+	if ( Z80_write_sequencer[1] )                 Z80_245_oe       <= 1'b1;				// enable 245 output
 
-	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_addr         <= Z80_addr[18:0];	// latch address bus onto GPU address bus
-	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_wdata        <= Z80_wData;			// latch data bus onto GPU data bus
-	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_wr_ena       <= 1'b1;					// turn on FPGA RAM we
+	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_addr         <= Z80_addr[18:0];// latch address bus onto GPU address bus
+	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_wdata        <= Z80_wData;		// latch data bus onto GPU data bus
+	if ( Z80_write_sequencer[DELAY_CYCLES + 1] )  gpu_wr_ena       <= 1'b1;				// turn on FPGA RAM we
 
-	if ( Z80_write_sequencer[DELAY_CYCLES + 2] )  gpu_wr_ena       <= 1'b0;					// turn off FPGA RAM we
-	if ( Z80_write_sequencer[DELAY_CYCLES + 2] )  Z80_245_oe       <= 1'b0;					// disable 245 output
+	if ( Z80_write_sequencer[DELAY_CYCLES + 2] )  gpu_wr_ena       <= 1'b0;				// turn off FPGA RAM we
+	if ( Z80_write_sequencer[DELAY_CYCLES + 2] )  Z80_245_oe       <= 1'b0;				// disable 245 output
 
-	if ( Read_GPU_RAM_BEGIN )
-	begin
-		gpu_addr				<= Z80_addr[18:0];// pass address to GPU RAM
+	if ( Read_GPU_RAM_BEGIN ) begin
+		
+		gpu_addr				<= Z80_addr[18:0];// pass address to GPU RAM (cropped to 512KB range as that's all we're interested in)
 		gpu_rd_req			<= 1'b1;				// flag a read request to the mux which is one-shotted in the mux
 		Z80_245data_dir	<= 1'b0;				// set 245 direction (TO Z80)
 		Z80_245_oe			<= 1'b1;				// enable 245 output
-	end else gpu_rd_req		<= 1'b0;					// end GPU read req after 1 pulse
-
-	if ( gpu_rd_rdy )	// gpu_rd_rdy is a one-shot from the mux, reset after one clock
-	begin
 		
-		//data_hold		<= 1'b1;					// latch the gpu_rd_rdy signal to keep outputting data until Z80 is done with it
+	end else begin
+		
+		gpu_rd_req			<= 1'b0;				// end GPU read req after 1 pulse
+		
+	end
+
+	if ( gpu_rd_rdy )	begin // gpu_rd_rdy is a one-shot from the mux, reset after one clock
+		
 		Z80_rData_ena	<= 1'b1;					// set bidir pins to output
-		//gpu_rd_req		<= 1'b0;					// End the read request once the read is ready
 		Z80_rData[7:0]	<= gpu_rData[7:0];	// Latch the GPU RAM read into the output register for the Z80
 		
 	end
 	
-	if ( Read_GPU_RAM_END )
-	begin
+	if ( Read_GPU_RAM_END ) begin
 		// data is being output to Z80, which has signalled end of read transaction
 		Z80_245_oe		<= 1'b0;					// disable 245 output
-		//data_hold		<= 1'b0;					// reset the latch
-		Z80_rData_ena	<= 1'b0;					// set bidir pins to input
+		Z80_rData_ena	<= 1'b0;					// re-set bidir pins to input
 	end
-
-	//Z80_245data_dir	<= GPU_data_oe;
-	//Z80_rData_ena	<= GPU_data_oe;
 	
-	last_Z80_WR	<= Z80_WRn;
-	last_Z80_RD	<= Z80_RDn;
-	Z80_clk_delay <= Z80_CLK;  // find the rising clock edge
+	last_Z80_WR		<= Z80_WRn;
+	last_Z80_RD		<= Z80_RDn;
+	Z80_clk_delay	<= Z80_CLK;					// find the rising clock edge
 
 end
+
+// **********************************************************************************************************
 
 endmodule
--- GPU_256char_font/data_mux.v
+++ GPU_256char_font/data_mux.v
@@ -1,40 +1,106 @@
+// 2-port data_mux
+//
+// Prioritises Port A for bi-directional data transfer,
+// holds requests from Port x for x cycles whilst another
+// request is in progress (x cycles = GPU RAM delay).
+//
+// All requests should take no longer than the specified
+// number of cycles to read data from the GPU RAM, but in
+// any case be quicker than the reading device attached
+// to this mux unit - i.e. the Z80 or RS232 interfaces.
+
 module data_mux (
 
 	// general inputs
-	input wire clk,
-	input wire reset,
+	input wire			clk,
+	input wire			reset,
 	
 	// gpu inputs
-	input wire rd_rdy,						// this should be HIGH for 1 clock once 'gpu_data_in[7:0]' contains valid data
-	input wire gpu_data_in[7:0],
+	input wire [7:0]	gpu_data_in,	// data is valid here once gpu_data_in pulses HIGH
 	
-	// inputs Port A
-	input wire wr_ena_a,
-	input wire rd_req_a,
-	input wire address_a[19:0],
-	input wire data_in_a[7:0],
+	// inputs Port A - Z80
+	input wire 			wr_ena_a,
+	input wire 			rd_req_a,
+	input wire [19:0]	address_a,
+	input wire [7:0]	data_in_a,
 	
 	
-	// inputs Port B
-	input wire wr_ena_b,
-	input wire rd_req_b,
-	input wire address_b[19:0],
-	input wire data_in_b[7:0],
+	// inputs Port B - RS232
+	input wire 			wr_ena_b,
+	input wire 			rd_req_b,
+	input wire [14:0]	address_b,
+	input wire [7:0]	data_in_b,
 	
 	// gpu outputs
-	output reg gpu_rd_req,					// this will pulse high for 1 clock when a read request takes place
-	output reg gpu_address[19:0],
-	output reg gpu_data_out[7:0],
+	output reg 			gpu_wr_ena,		// output pulses high for 1 clock when a write request takes place
+	output reg [19:0]	gpu_address,
+	output reg [7:0]	gpu_data_out,
 	
 	// outputs Port A
-	output reg gpu_rd_rdy_a,
-	output reg data_out_a[7:0],
+	output wire 		gpu_rd_rdy_a,	// one-CLK HIGH pulse here indicates valid data on data_out_a
+	output wire [7:0]	data_out_a,
 	
 	// outputs Port B
-	output reg gpu_rd_rdy_b,
-	output reg data_out_b[7:0]
+	output wire 		gpu_rd_rdy_b,	// one-CLK HIGH pulse here indicates valid data on data_out_b
+	output wire [7:0]	data_out_b
 
 );
 
+parameter DELAY_CYCLES = 2;
+
+reg [9:0] rd_sequencer_a;
+reg [9:0] rd_sequencer_b;
+
+reg porta_bsy, portb_bsy;
+
+wire run_r_porta, run_r_portb, run_w_porta, run_w_portb;
+
+assign run_r_porta	= rd_req_a && ~portb_bsy;
+assign run_w_porta	= wr_ena_a && ~portb_bsy;
+assign run_r_portb	= rd_req_b && ~porta_bsy && ~run_r_porta && ~run_w_porta;
+assign run_w_portb	= wr_ena_b && ~porta_bsy && ~run_r_porta && ~run_w_porta;
+
+assign gpu_rd_rdy_b	= rd_sequencer_b[DELAY_CYCLES];
+assign gpu_rd_rdy_a	= rd_sequencer_a[DELAY_CYCLES];
+
+assign data_out_a		= gpu_data_in ;
+assign data_out_b		= gpu_data_in ;
+
+always @ (posedge clk) begin
+
+	// *** UPDATE GPU_WR_ENA AND FLAGS EACH CLOCK ***
+	gpu_wr_ena	<= run_w_porta || run_w_portb;
+	porta_bsy	<= run_r_porta || run_w_porta;
+	portb_bsy	<= run_r_portb || run_w_portb;
+
+	// *** HANDLE PORT A READ REQUESTS AND SEQUENCER ***
+	if (run_r_porta) begin
+		rd_sequencer_a[9:0] <= { rd_sequencer_a[8:0], 1'b1 };
+		gpu_address		     <= address_a;
+	end else begin
+		rd_sequencer_a[9:0] <= { rd_sequencer_a[8:0], 1'b0 };	// this line must always run no matter any other state
+	end
+	
+	// *** HANDLE PORT A WRITE REQUESTS ***
+	if (run_w_porta) begin
+		gpu_address		     <= address_a;
+		gpu_data_out        <= data_in_a;
+	end
+	
+	// *** HANDLE PORT B READ REQUESTS AND SEQUENCER ***
+	if (run_r_portb) begin
+		rd_sequencer_b[9:0] <= { rd_sequencer_b[8:0], 1'b1 };
+		gpu_address		     <= address_b;
+	end else begin
+		rd_sequencer_b[9:0] <= { rd_sequencer_b[8:0], 1'b0 };	// this line must always run no matter any other state
+	end
+	
+	// *** HANDLE PORT B WRITE REQUESTS ***
+	if (run_w_portb) begin
+		gpu_address		     <= address_b;
+		gpu_data_out        <= data_in_b;
+	end
+	
+end
 
 endmodule
